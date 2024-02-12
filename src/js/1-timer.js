d7752938babff1363dd8837e878d51005506b1c3
Yehor Tolstiakov <88378124+Alf-540@users.noreply.github.com>
2024-02-12 13:36:43 +0200

    summary

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,15 +1,57 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const btnStart = document.querySelector('button');
+const input = document.querySelector('input');
+const day = document.querySelector('.value[ data-days]');
+const hour = document.querySelector('.value[ data-hours]');
+const minute = document.querySelector('.value[ data-minutes]');
+const second = document.querySelector('.value[ data-seconds]');
+btnStart.disabled = true;
+let date = Date.now();
+let userSelectedDate;
+let difference;
+let setIntervalId;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate < Date.now()) {
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        messageColor: '#FFFFFF',
+        backgroundColor: '#B51B1B',
+        position: 'topRight',
+      });
+    } else {
+      btnStart.disabled = false;
+      btnStart.style.background = '#4E75FF';
+      btnStart.style.color = '#FFF';
+    }
   },
 };
+
+flatpickr('#datetime-picker', options);
+
+btnStart.addEventListener('click', e => {
+  btnStart.disabled = true;
+  input.disabled = true;
+  btnStart.style.background = '#CFCFCF';
+  btnStart.style.color = '#989898';
+  difference = userSelectedDate - Date.now();
+  timerNumber(convertMs(difference));
+  setIntervalId = setInterval(() => {
+    difference -= 1000;
+    timerNumber(convertMs(difference));
+    stopTimer(difference);
+  }, 1000);
+});
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -18,18 +60,27 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function timerNumber({ days, hours, minutes, seconds }) {
+  day.textContent = `${addLeadingZero(days)}`;
+  hour.textContent = `${addLeadingZero(hours)}`;
+  minute.textContent = `${addLeadingZero(minutes)}`;
+  second.textContent = `${addLeadingZero(seconds)}`;
+}
+
+function stopTimer(difference) {
+  if (difference <= 1000) {
+    clearInterval(setIntervalId);
+  }
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
